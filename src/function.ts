32bd2dd6b73d31919bd3fdd9e40dae65b9f89276
j-khong <julienkhong@yahoo.fr>
2020-03-30 14:53:11 +0200

    1.3.2

--- src/function.ts
+++ src/function.ts
@@ -1,10 +1,10 @@
 import { DevUtils } from './index';
 
 export class FunctionUtils {
-  public static isAsync(fn: any): boolean {
-    if (DevUtils.isNotSet(fn)) {
-      return false;
-    }
-    return (fn.toString().includes('__awaiter')) || (fn.constructor.name === 'AsyncFunction');
-  }
+   public static isAsync(fn: any): boolean {
+      if (DevUtils.isNotSet(fn)) {
+         return false;
+      }
+      return fn.toString().includes('__awaiter') || fn.constructor.name === 'AsyncFunction';
+   }
 }
